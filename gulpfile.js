c5ecc1f737cc239881e45d3c75659d07bb86aea5
Valerie Liang <liang.val63@gmail.com>
2016-03-09 08:31:32 -0800

    Added automated workflow for compilation from JSX to JS for React and Less to CSS for Less.

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var babelify = require('babelify');
+var react = require('babel-preset-react')
+var concat = require('gulp-concat')
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var transform = require('transform');
+
+
+function swallowError (error) {
+    console.log(error.toString());
+    this.emit("end");
+}
+
+
+gulp.task('browserify', function() {
+	return browserify({
+		entries:'./client/app.js',
+		debug: true
+	})
+	.transform(babelify.configure({presets:"react"}))
+	.on('error', swallowError)	
+	.bundle()
+	.on('error', swallowError)
+	.pipe(source('bundle.js'))
+	.on('error', swallowError)
+	.pipe(gulp.dest('./client/dist'))
+});
+
+gulp.task('lessify', function() {
+	return gulp.src(['./client/styles.less'])
+	.pipe(concat('styles.css'))
+	.pipe(less())
+	.on('error', swallowError)
+	.pipe(gulp.dest('./client/dist'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./client/app.js', ['browserify']);
+	gulp.watch('./client/styles.less', ['lessify']);
+});
+
+gulp.task('build', ['browserify', 'lessify', 'watch']);

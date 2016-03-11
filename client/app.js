e8322f656e1edc6531bce1cf28e8dca5cbc7a104
Valerie Liang <liang.val63@gmail.com>
2016-03-11 02:19:22 -0800

    Added game over and next steps logic - todo: fix minimax function.

--- client/app.js
+++ client/app.js
@@ -6,41 +6,47 @@ var Game = React.createClass({
 	render: function () {
 		return (
 			<div className="container">
-				<Title />
+				<h1 className="game-title">Tic-Tac-Toe</h1>
 				<GameBoard />
 			</div>
 		)	
 	}
 });
 
-var Title = React.createClass({
-	render: function () {
-		return (
-			<h1 className="game-title">Tic-Tac-Toe</h1>
-		)
-	}
-});
-
 var GameBoard = React.createClass({
 	getInitialState: function() {
 		return {
-			"boardData": [["X","","O"],["","",""],["","",""]],
+			"boardData": [["","",""],["","",""],["","",""]],
 			"currentPlayer":"X"
 		}
 	},
 	handlePieceMove: function(address){
-		console.log('click heard')
-		console.log(address)
-		console.log(this.state.currentPlayer)
+		console.log('handling piece move')
 		var addressLoc = address.split(",")
 		var boardData = this.state.boardData
 		boardData[addressLoc[0]][addressLoc[1]] = this.state.currentPlayer;
-		console.log(boardData[addressLoc[0]][addressLoc[1]])
+		gameOverStatus = gameOver(boardData)
+		if (gameOverStatus[0]){
+			console.log('game over')
+			console.log(gameOverStatus[1])
+			this.setState({boardData:boardData})
+			// Create user scoreboard
+			// Create game over alert
+			return;
+		}
 		var nextPlayer = this.state.currentPlayer === "X" ? "O" : "X";
 		this.setState({
 			"boardData": boardData,
 			"currentPlayer": nextPlayer
-		})
+		})		
+	},
+	componentDidUpdate: function() {
+		console.log('componenetdidupdate')
+		console.log(this.state.currentPlayer)
+		if(this.state.currentPlayer === "O") {
+			console.log('handling move')
+			setTimeout(function(){this.handlePieceMove(getNextMove(this.state.boardData, this.state.currentPlayer).join(","))}.bind(this),1000)
+		}
 	},
 	render: function() {
 		return (
@@ -67,12 +73,14 @@ var GameBoard = React.createClass({
 	}
 });
 
+// var GameOverAlert = React.createClass({
+// 	return (
+// 		<
+// 	)
+// });
+
 var BoardSquare = React.createClass({
 	handlePieceMove: function() {
-		console.log('status')
-		console.log(this.props.status)
-		console.log(this.props.player)
-		console.log(this.props.status !== this.props.player)
 		if (!this.props.status && this.props.status !== this.props.player){
 			console.log('moving piece')
 			this.props.handlePieceMove(this.props.address)
@@ -85,15 +93,84 @@ var BoardSquare = React.createClass({
 	}
 });
 
-var PlayerPiece = React.createClass({
-	render: function () {
-	}
-});
+/// Functions here to be moved out of file eventually and required.///
 
-var OpponentPiece = React.createClass({
-	render: function () {
-	}
-});
+function findMaxMin (array, maxOrMin) {
+  var index = maxOrMin === "max" ? array.length - 1 : 0;
+  array.sort(function(x,y) {return x - y;});
+  return array[index];
+};
+
+function gameOver (board) {
+  var fullBoard = "";
+  for (var x = 0; x < 3; x++) {
+    //check horizontals
+    if ((board[x][0]) && (board[x][0] === board[x][1] && board[x][1] === board[x][2])) {
+      return [true, board[x][0]];
+    }
+    //check verticals
+    if ((board[0][x]) && (board[0][x] === board[1][x] &&  board[1][x] === board[2][x])) {
+      return [true, board[0][x]];
+    }
+    fullBoard += board[x][0] + board[x][1] + board[x][2];
+  }
+  //check diagonals
+  if ((board[0][0]) && (board[0][0] === board[1][1] && board[1][1] === board[2][2])) {
+    return [true, board[0][0]];
+  }
+  if ((board[0][2]) && (board[0][2] === board[1][1] && board[1][1] === board[2][0])) {
+    return [true, board[0][2]];
+  }
+  // check full board
+  if (fullBoard.length === 9) {
+    return [true, "Draw"]
+  }
+  return [false, ""];
+}
+
+function getNextMove (board, player) {
+  var choice = ""
+  function minimax(board, player) {
+    var gameOverState = gameOver(board);
+    //base cases
+    if (gameOverState[0]) {
+      if (gameOverState[1] != "Draw") {
+        return gameOverState[1] === "X" ? 10 : -10;
+      } else {
+        return 0;
+      }
+    }
+    
+    var scores = [];
+    var moves = [];
+
+    for (var i = 0; i < board.length; i++) {
+      for (var j = 0; j < board[i].length; j++) {
+        if (!board[i][j]) {
+          board[i][j] = player;
+          scores.push(minimax(board, player === "X" ? "O" : "X"));
+          moves.push([i,j])
+          board[i][j] = "";
+        }
+      }
+    }
+
+    if (player === "X") {
+      var maxScoreIndex = scores.indexOf(findMaxMin(scores, "max"));
+      choice = moves[maxScoreIndex] 
+      return scores[maxScoreIndex]
+    }
+    if (player === "O") {
+      var minScoreIndex = scores.indexOf(findMaxMin(scores, "min"));
+      choice = moves[minScoreIndex]
+      return scores[minScoreIndex]
+    }
+    
+  }
+  minimax(board, player)
+  return choice
+}
+
 
 
 ReactDOM.render(
